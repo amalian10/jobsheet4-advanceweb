ad48433f9cc69480cfd6ebd1c16f6b11cae550f9
amalian10 <2041720160@student.polinema.ac.id>
2022-03-21 21:11:48 +0700

    Create abouts table in migration

---
+++ database/migrations/2022_03_19_160730_create_abouts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAboutsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('abouts', function (Blueprint $table) {
+            $table->id();
+            $table->text('image', 255);
+            $table->text('title', 150);
+            $table->longText('deskripsi1');
+            $table->longText('deskripsi2');
+            $table->longText('deskripsi3');
+            $table->longText('deskripsi4');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('abouts');
+    }
+}
